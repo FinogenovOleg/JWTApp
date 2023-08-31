4351ce0bb7b328301ddaff42c42765f800d09177
FinogenovOleg <finogenov_oleg2000@mail.ru>
2023-08-31 18:04:05 +0300

    JWPApp
    JWPApp

---
+++ JWTApp/src/test/java/ru/finogenov/springcourse/FirstSecurityApp/SpringSecurityAppTests.java
@@ -0,0 +1,13 @@
+package ru.finogenov.springcourse.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
